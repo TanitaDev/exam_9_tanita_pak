6a82ca86c795b04c35147878892dba5a2b09efd3
tanitaDev <tanita.pak@gmail.com>
2022-11-26 12:52:06 +0600

    написала модель для фото и для избранного

--- app/webapp/admin.py
+++ app/webapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import Photo, Favorite
+
+
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'image', 'signature', 'author', 'created_at']
+
+
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'image']
+
+
+admin.site.register(Photo, PhotoAdmin)
+admin.site.register(Favorite, FavoriteAdmin)
--- app/webapp/models.py
+++ app/webapp/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Photo(models.Model):
+    image = models.ImageField(verbose_name='Фото', upload_to='uploads/img', blank=False, null=False)
+    signature = models.CharField(verbose_name='Подпись', max_length=1000, blank=False, null=False)
+    created_at = models.DateTimeField(verbose_name="Время создания", auto_now_add=True)
+    author = models.ForeignKey(
+        verbose_name='Автор', to=get_user_model(), related_name='photos', on_delete=models.CASCADE, blank=False,
+        null=False
+    )
+
+
+class Favorite(models.Model):
+    user = models.ForeignKey(to=User, related_name='favorite', verbose_name='Пользователь', null=False,
+                             on_delete=models.CASCADE)
+    image = models.ForeignKey(to=Photo, related_name='favourite_users', verbose_name='Избранное', null=False,
+                              on_delete=models.CASCADE)
